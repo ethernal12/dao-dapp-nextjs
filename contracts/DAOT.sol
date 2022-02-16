0c68a02d6342907674ea098f7d5bca5946356e99
ethernal12 <31479093+ethernal12@users.noreply.github.com>
2022-02-16 18:12:00 +0100

    Update DAOT.sol

--- contracts/DAOT.sol
+++ contracts/DAOT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
-//import   "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0/contracts/token/ERC20/ERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
     contract DAOT is ERC20{
